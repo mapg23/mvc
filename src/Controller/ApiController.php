cd3db072f8bf3f8a4b46d8f3b137c75de1c343fe
mapg23 <mapg23@student.bth.se>
2024-03-29 21:30:21 +0100

    quotes

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -15,4 +15,27 @@ class ApiController extends AbstractController
         return $this->render("api.html.twig");
     }
 
+    #[Route("/api/quote", name: "quote")]
+    public function quote() : Response
+    {
+        $numb = random_int(0,2);
+
+        $quotes = array(
+            "Code dosent lie, comments sometimes do.",
+            "Good software, like wine, takes time.",
+            "If, at first, you do not succeed, call it version 1.0."
+        );
+
+        $data = [
+            'quote' => $quotes[$numb],
+            'date' => date("Y-m-d"),
+            'generated' => date('Y-m-d H:i:s', time())
+        ];
+
+        $response = new Response();
+        $response->setContent(json_encode($data));
+        $response->headers->set('Content-Type', 'application/json');
+        
+        return $response;
+    }
 }
